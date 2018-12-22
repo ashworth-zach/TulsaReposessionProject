7398218a8b0c0491b8fc0b1622bfa639f34369af
Zach Ashworth <zashworth2@gmail.com>
2018-12-22 12:49:57 -0600

    moving js

--- static/mainapp/js/script.js
+++ static/mainapp/js/script.js
@@ -1,3 +1,21 @@
+$('.ajax_form').submit(function (e) {
+    e.preventDefault()
+})
+$('#ajax_tag').keyup(function () {
+    console.log('Sending Ajax request to /find')
+    console.log('Submitting the following data', $(this).parent().serialize())
+    $.ajax({
+        url: '/find',
+        /* Where should this go? */
+        method: 'post',
+        /* Which HTTP verb? */
+        data: $(this).parent().serialize(),
+        /* Any data to send along? */
+        success: function (serverResponse) { /* What code should we run when the server responds? */
+            $('#mytable').html(serverResponse)
+        }
+    })
+});
 var table = '#mytable'
 $('#maxRows').on('change', function () {
     $('.pagination').html('')
@@ -43,3 +61,62 @@ $(function () {
         $(this).prepend('<td>' + id + '</td>')
     })
 })
+navigator.getUserMedia = (navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia);
+
+var video;
+var webcamStream;
+var x;
+function startWebcam() {
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia(
+
+            // constraints
+            {
+                video: true,
+                audio: false
+            },
+
+            // successCallback
+            function (localMediaStream) {
+                video = document.querySelector('video');
+                video.src = window.URL.createObjectURL(localMediaStream);
+                webcamStream = localMediaStream;
+            },
+
+            // errorCallback
+            function (err) {
+                console.log("The following error occured: " + err);
+            }
+        );
+    } else {
+        console.log("getUserMedia not supported");
+    }
+}
+
+function stopWebcam() {
+    webcamStream.stop();
+}
+//---------------------
+// TAKE A SNAPSHOT CODE
+//---------------------
+var canvas, ctx;
+
+function init() {
+    // Get the canvas and obtain a context for
+    // drawing in it
+    canvas = document.getElementById("myCanvas");
+    ctx = canvas.getContext('2d');
+}
+
+function snapshot() {
+    // Draws current image from the video element into the canvas
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+}
+function upload() {
+    var result = $('#myCanvas')[0].toDataURL();
+    var strImage = result.replace(/^data:image\/[a-z]+;base64,/, "");
+    $('#canvasdata')[0].value = strImage
+}
